df614dc21473273ff9f28f3d6c77b3e38a0346a7
Fahad <mfah@ciklum.com>
2020-09-22 12:54:12 +0500

    added singleton pattern

---
+++ Singleton Pattern/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_Pattern
+{
+    public class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
